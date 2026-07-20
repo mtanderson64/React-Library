8e87dae0a16a30af926e683ad02889fbfd5966e7
Mark Anderson <mtandereson64@gmail.com>
2026-07-20 14:52:33 -0500

    stuck on add to cart tutorial timestamp 22:30

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -3,7 +3,7 @@ import Book from './ui/Book';
 import { books } from '../data'
 
  const Featured = () => {
-   console.log(books);
+   //console.log(books);
 /*    function getFiveStarBooks() {
       books.filter(book => book.rating === 5).slice(0, 4);
    } */
